bd862cfa967fa7407945ef6f583ae6d6e15f7c4c
jettangs <jettangs@gmail.com>
2017-08-13 21:12:20 +0800

    initial

--- lanxess.js
+++ lanxess.js
@@ -1,7 +1,7 @@
 exports.news = {
 	body: `$('.lxs-list-content')`,
 	title: `find('.headline').find('a').html()`,
-	content: `$(".container-content").html()`,
+	content: `$(".lxs-news-detail").html()`,
 	description: `find('.bodytext').html()`,
 	link: `find('.headline').find('a').attr('href')`,
 	cover: `.find('.lxs-list-pic').find('img').attr('src')`,
